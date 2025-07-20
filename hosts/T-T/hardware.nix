c07a75d98f4b4e61483295990842e34d760f591e
1X1Y-SN <ngynst@gmail.com>
2025-07-20 18:00:51 -0400

    Create hardware.nix

---
+++ hosts/T-T/hardware.nix
@@ -0,0 +1,21 @@
+{config, ...}: {
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement = {
+        enable = false;
+        finegrained = false;
+      };
+      open = true;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+    nvidia-container-toolkit = {
+      enable = true;
+    };
+  };
+}
