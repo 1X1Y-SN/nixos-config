278cf9ecfce79b12170c50ed3c8118fbfdd738e5
1X1Y-SN <ngynst@gmail.com>
2025-07-20 18:17:50 -0400

    Update configuration.nix

--- hosts/T-T/configuration.nix
+++ hosts/T-T/configuration.nix
@@ -1,20 +1,19 @@
 {...}: {
+  system = {
+    stateVersion = "25.05";
+  };
+
   imports = [
     ./boot.nix
     ./disko-config.nix
     ./filesystems.nix
     ./hardware.nix
     ./hardware-configuration.nix
+    ./locale.nix
     ./networking.nix
+    ./nix.nix
     ./packages.nix
     ./services.nix
     ./users.nix
   ];
-
-  system.stateVersion = "25.05";
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  time.timeZone = "America/Toronto";
-  i18n.defaultLocale = "en_CA.UTF-8";
 }
