548abcb82273a660b7b111e47374b097a31caaa4
1X1Y-SN <ngynst@gmail.com>
2025-07-20 07:57:35 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
